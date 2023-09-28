cc4f70da1f2f835ce0f5fd179cf33106073ed093
william <ofeliavm69@gmail.com>
2023-09-28 19:23:07 -0400

    Añadiendo un comentario

--- EjercicioGit/Program.cs
+++ EjercicioGit/Program.cs
@@ -10,6 +10,9 @@ namespace EjercicioGit
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("En clases ");
+            Console.ReadKey();
         }
     }
 }
